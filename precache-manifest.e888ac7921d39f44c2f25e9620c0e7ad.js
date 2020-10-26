ace8df45259f4edf73bd9102e7b0aad3ff7c15b4
vakhno <vakhnohere@gmail.com>
2020-10-26 05:12:53 +0200

    Updates

---
+++ precache-manifest.e888ac7921d39f44c2f25e9620c0e7ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895615c5141f72c62d343a683b33d9ef",
+    "url": "/Task-manager-React/index.html"
+  },
+  {
+    "revision": "37920b001ff20f75f946",
+    "url": "/Task-manager-React/static/css/main.7e00da84.chunk.css"
+  },
+  {
+    "revision": "4afea160b61b18f49e97",
+    "url": "/Task-manager-React/static/js/2.bce0bf0d.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/Task-manager-React/static/js/2.bce0bf0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37920b001ff20f75f946",
+    "url": "/Task-manager-React/static/js/main.84dc3983.chunk.js"
+  },
+  {
+    "revision": "71e6737cb3391424689b",
+    "url": "/Task-manager-React/static/js/runtime-main.a12ef6a8.js"
+  },
+  {
+    "revision": "77a10ed42fa83d8e366a91e809a921ab",
+    "url": "/Task-manager-React/static/media/background.77a10ed4.jpg"
+  }
+]);
